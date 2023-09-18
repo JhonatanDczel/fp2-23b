3b6c3c402b581ca43216641e6ab811dad6007017
JhonatanDczel <jariasq@unsa.edu.pe>
2023-09-18 08:23:46 -0500

    Actividad 1

---
+++ fase01/lab01/Soldado.java
@@ -0,0 +1,10 @@
+public class Soldado{
+  public String name;
+
+  public void setName(String str){
+    this.name = str;
+  }
+  public String getName(){
+    return this.name;
+  }
+}
---
+++ fase01/lab01/VideoJuego.java
@@ -0,0 +1,9 @@
+public class VideoJuego{
+  public static void main(String[] args){
+    Soldado rambo = new Soldado();
+    rambo.setName("rambo");
+    System.out.println(rambo.getName());
+
+  }
+
+}
