2f3ca47fa45c5bc8aa42dd447b0f19f1b6b95cad
JhonatanDczel <jariasq@unsa.edu.pe>
2023-09-20 08:57:57 -0500

    Actividad 2: creacion de 5 soldados con el nivel de vida, y ajuste del codigo para poder ingresar los datos de cada uno y luego mostrarlos en pantalla

--- fase01/lab01/Soldado.java
+++ fase01/lab01/Soldado.java
@@ -1,11 +1,24 @@
 public class Soldado{
   public String name;
+  public int life;
 
-  public Soldado(String str){
-    this.name = str;
+  public Soldado(String name){
+    this.name = name;
   }
+
+  //SECCION DE SETERS
+
+  public void setLife(int life){
+    this.life = life;
+  }
+
+  //SECCION DE GETERS
 
   public String getName(){
     return this.name;
+  }
+
+  public int getLife(){
+    return this.life;
   }
 }
--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -1,12 +1,47 @@
+// Laboratorio Nro 1 - Ejercicios
+// Autor: Arias Quispe Jhonatan David
+// Colaboro: --------
+// Tiempo: 30 minutos
+import java.util.*;
+
 public class VideoJuego{
+
   public static void main(String[] args){
-    Soldado rambo = new Soldado("Rambo");
-    Soldado goomp = new Soldado("Goomp");
-    Soldado joel = new Soldado("Joel");
-    Soldado mauricio = new Soldado("Mauricio");
-    Soldado rogelio = new Soldado("Rogelio");
+    Scanner sc = new Scanner(System.in);
     
-    System.out.println("Escuadron Sistemas:\n" + rambo.getName() + "\n" + goomp.getName() + "\n" + joel.getName() + "\n" + mauricio.getName() + "\n" + rogelio.getName() + "\n");
+    System.out.println("Ingrese el nombre del nuevo soldado:");
+    Soldado s1 = new Soldado(sc.next());
+    System.out.println("Ingrese el nivel de vida del nuevo soldado:");
+    s1.setLife(sc.nextInt());
+
+
+    System.out.println("Ingrese el nombre del nuevo soldado:");
+    Soldado s2 = new Soldado(sc.next());
+    System.out.println("Ingrese el nivel de vida del nuevo soldado:");
+    s2.setLife(sc.nextInt());
+
+    System.out.println("Ingrese el nombre del nuevo soldado:");
+    Soldado s3 = new Soldado(sc.next());
+    System.out.println("Ingrese el nivel de vida del nuevo soldado:");
+    s3.setLife(sc.nextInt());
+
+    System.out.println("Ingrese el nombre del nuevo soldado:");
+    Soldado s4 = new Soldado(sc.next());
+    System.out.println("Ingrese el nivel de vida del nuevo soldado:");
+    s4.setLife(sc.nextInt());
+
+    System.out.println("Ingrese el nombre del nuevo soldado:");
+    Soldado s5 = new Soldado(sc.next());
+    System.out.println("Ingrese el nivel de vida del nuevo soldado:");
+    s5.setLife(sc.nextInt());
+
+    System.out.println("=====DATOS DE SOLDADOS=====");
+    System.out.println("Soldado: " + s1.getName() + " \nNivel de vida: " + s1.getLife() + "\n");
+    System.out.println("Soldado: " + s2.getName() + " \nNivel de vida: " + s2.getLife() + "\n");
+    System.out.println("Soldado: " + s3.getName() + " \nNivel de vida: " + s3.getLife() + "\n");
+    System.out.println("Soldado: " + s4.getName() + " \nNivel de vida: " + s4.getLife() + "\n");
+    System.out.println("Soldado: " + s5.getName() + " \nNivel de vida: " + s5.getLife() + "\n");
+
   }
 
 }
