df4708da398d55209f12efcf96462027506db4f1
JhonatanDczel <jariasq@unsa.edu.pe>
2023-09-20 09:41:57 -0500

    Actividad 5: creacion del metodo display, y pruebas de todos los metodos implementados hasta ahora

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -7,8 +7,25 @@ import java.util.*;
 public class VideoJuego{
 
   public static void main(String[] args){
+    String[] army1 = new String[5];
+    String[] army2 = new String[5];
+
+    initializeArmy(army1);
+    initializeArmy(army2);
+
+    displayArmy(army1);
+    displayArmy(army2);
 
   }
+
+
+  public static void displayArmy(String[] army){
+    System.out.println("\n===== Army Soldiers =====");
+    for(String soldier : army){
+      System.out.println(" " + soldier);
+    }
+  }
+
   public static void initializeArmy(String[] army){
     Random rand = new Random();
     int randNum = rand.nextInt(5) + 1;
