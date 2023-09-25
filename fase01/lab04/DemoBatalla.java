66cf73eb51e08b1a48c531dd3861805b416e97c4
JhonatanDczel <jariasq@unsa.edu.pe>
2023-09-25 18:29:20 -0500

    Actividad 1: Implementacion del metodo Busqueda Lineal

---
+++ fase01/lab04/DemoBatalla.java
@@ -0,0 +1,126 @@
+import java.util.*;
+public class DemoBatalla {
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String [] args){
+    Nave [] misNaves = new Nave[3];
+    String nomb, col;
+    int fil, punt;
+    boolean est;
+    for (int i = 0; i < misNaves.length; i++) {
+      System.out.println("Nave " + (i+1));
+      System.out.print("Nombre: ");
+      nomb = sc.next();
+      System.out.print("Fila: ");
+      fil = sc.nextInt();
+      System.out.print("Columna: ");
+      col = sc.next();
+      System.out.print("Estado: ");
+      est = sc.nextBoolean();
+      System.out.print("Puntos: ");
+      punt = sc.nextInt();
+      misNaves[i] = new Nave(); //Se crea un objeto Nave y se asigna su referencia a misNaves
+      misNaves[i].setNombre(nomb);
+      misNaves[i].setFila(fil);
+      misNaves[i].setColumna(col);
+      misNaves[i].setEstado(est);
+      misNaves[i].setPuntos(punt);
+      System.out.println();
+    }
+    System.out.println("\nNaves creadas:");
+    mostrarNaves(misNaves);
+    desordenar(misNaves);
+    mostrarPorNombre(misNaves);
+    mostrarPorPuntos(misNaves);
+    System.out.println("\nNave con mayor número de puntos: " + mostrarMayorPuntos(misNaves));
+  }
+//Método para mostrar todas las naves
+  public static void mostrarNaves(Nave [] flota){
+    System.out.println();
+    int i = 1;
+    for(Nave n : flota){
+      System.out.println("Nave " + i + ":");
+      mostrarNave(n);
+      System.out.println();
+      i++;
+    } 
+  }
+//Método para mostrar todas las naves de un nombre que se pide por teclado
+  public static void mostrarPorNombre(Nave [] flota){
+    System.out.println("\nMostrar naves por nombre, ingrese un nombre:\n");
+    String nombre = sc.next().toLowerCase();
+    int i = 1;
+    for(Nave n : flota){
+      if(n.getNombre().toLowerCase().equals(nombre)){
+        System.out.println("Nave " + i + ":");
+        mostrarNave(n);
+        System.out.println();
+        i++;
+      }
+    }
+    if(i == 1){
+      System.out.println("No se han encontrado naves con ese nombre");
+      System.out.println();
+    }
+
+  }
+//Método para mostrar todas las naves con un número de puntos inferior o igual
+//al número de puntos que se pide por teclado
+  public static void mostrarPorPuntos(Nave [] flota){
+    System.out.println("\nMostrar naves por puntos, ingrese una cantidad:\n");
+    int pts = sc.nextInt();
+    int i = 1;
+    for(Nave n : flota){
+      if(n.getPuntos() <= pts){
+        System.out.println("Nave " + i + ":");
+        mostrarNave(n);
+        System.out.println();
+        i++;
+      }
+    }
+    if(i == 1){
+      System.out.println("No se han encontrado naves");
+      System.out.println();
+    }   
+  }
+//Método que devuelve la Nave con mayor número de Puntos
+  public static Nave mostrarMayorPuntos(Nave [] flota){
+    Nave mayor = flota[0];
+    for(int i = 0; i < flota.length; i++){
+      if(flota[i].getPuntos() > mayor.getPuntos())
+        mayor = flota[i];
+    }
+    return mayor;
+  }
+  public static void mostrarNave(Nave nave){
+    System.out.println("Nombre: " + nave.getNombre());
+    System.out.println("Estado: " + nave.getEstado());
+    System.out.println("Puntos: " + nave.getPuntos());
+  }
+//Crear un método que devuelva un nuevo arreglo de objetos con todos los objetos previamente ingresados
+//pero aleatoriamente desordenados
+  public static Nave[] desordenar(Nave[] flota){
+    System.out.println("\nDesordenando las naves:\n");
+    Random random = new Random();
+    Nave[] nuevaFlota = new Nave[flota.length];
+    
+    for(int i = 0; i < flota.length; i++){
+      boolean ubicado = false;
+      while(!ubicado){
+        int numRandom = random.nextInt(flota.length);
+        if(nuevaFlota[numRandom] == null){
+          nuevaFlota[numRandom] = flota[i];
+          ubicado = true;
+          System.out.println("Nave " + i + " ahora ubicada en: " + numRandom);
+        }
+      }
+    }
+    return nuevaFlota;
+  } 
+  public static int busquedaLinealNombre(Nave[] flota, String s){
+    for(int i = 0; i < flota.length; i++){
+      if(flota[i].getNombre().equals(s))
+        return i;
+    }
+    return -1;
+  }
+}
