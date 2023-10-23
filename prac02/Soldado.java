9727fb5a86769ae49da39d68d26a9cb2a9fd7e8a
JhonatanDczel <jariasq@unsa.edu.pe>
2023-10-23 16:10:43 -0500

    PRAC02: COPIANDO

---
+++ prac02/Soldado.java
@@ -0,0 +1,47 @@
+public class Soldado{
+  public String name;
+  public int life;
+  public int row;
+  public int column;
+  public boolean negro = false;
+
+  public Soldado(String name){
+    this.name = name;
+  }
+
+  //SECCION DE SETERS
+
+  public void setNegro(boolean n){
+    this.negro = n;
+  }
+  public void setLife(int life){
+    this.life = life;
+  }
+  public void setColumn(int x){
+    this.column = x;
+  }
+  public void setRow(int y){
+    this.row = y;
+  }
+
+  //SECCION DE GETERS
+
+  public boolean isNegro(){
+    return this.negro;
+  }
+  public String getName(){
+    return this.name;
+  }
+
+  public int getLife(){
+    return this.life;
+  }
+  
+  public int getColumn(){
+    return this.column;
+  }
+
+  public int getRow(){
+    return this.row;
+  }
+}
