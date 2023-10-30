f77399934672905a999000438912710b6d445709
JhonatanDczel <jariasq@unsa.edu.pe>
2023-10-30 14:32:26 -0500

    Generador de ejercitos de soldado con nombre autogenerado

--- fase02/lab09/VideoJuego.java
+++ fase02/lab09/VideoJuego.java
@@ -6,5 +6,10 @@ public class VideoJuego{
   }
   public static Soldado[] generarEjercito(){
     Random rand = new Random();
+    int numSoldados = rand.nexInt(10) + 1;
+    Soldado[] ejercito = new Soldado[numSoldados];
+    for(int i = 0; i < numSoldados; i++){
+      ejercito[i] = new Soldado("Soldado " + n + "x" + i, rand.nexInt(5) + 1)
+    }
   }
 }
