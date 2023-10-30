64e314029a137154fd22b1a0f44c744bf8b988de
JhonatanDczel <jariasq@unsa.edu.pe>
2023-10-30 10:19:06 -0500

    Implementa atributos de la clase Soldado
    Se agregaron los atributos en modo private ya que se accederan a estos
    mediante los metodos accesores

--- fase02/lab09/Soldado.java
+++ fase02/lab09/Soldado.java
@@ -1,3 +1,10 @@
 public class Soldado {
-
+  private String nombre;
+  private int nivelAtaque;
+  private int nivelDefensa;
+  private int nivelVida;
+  private int nivelActual;
+  private int velocidad;
+  private String actitud;
+  private boolean vive;
 }
