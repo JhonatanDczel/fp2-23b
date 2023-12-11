02e7ef0f37f7876792ccf3682f84824cd090e05a
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-11 17:04:24 -0500

    Lógica inicial para el bucle del juego

--- fase02/lab12/Videojuego.java
+++ fase02/lab12/Videojuego.java
@@ -1,5 +1,10 @@
 import java.util.*;
 
 public class VideoJuego {
-
+  public static void juegoRapido() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Para salir de la partida ingresa 'S'");
+    String salir = sc.next();
+    if(salir.toLowerCase().charAt(0) == 's') return;
+  }
 }
