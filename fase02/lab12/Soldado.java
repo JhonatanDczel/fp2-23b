a7fb4575d45eaadc5f8a62a5e5baaa9c956c53a4
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-11 16:55:17 -0500

    Atributos para la clase Soldado

---
+++ fase02/lab12/Soldado.java
@@ -0,0 +1,13 @@
+public class Soldado {
+  private String nombre;
+  private int fila;
+  private int nivelAtaque = random(5);
+  private int nivelDefensa = random(5);
+  private int columna;
+  private int nivelVida;
+  private int vidaActual;
+  private int velocidad;
+  private String actitud;
+  private boolean vive;
+  private String team;
+}
