b7aecfc012fda83b168a8a0b318f4e80f528d68f
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-11 17:12:36 -0500

    Metodo main

--- fase02/lab12/Videojuego.java
+++ fase02/lab12/Videojuego.java
@@ -198,5 +198,17 @@ public class VideoJuego {
   public static void volver() {
 
   }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      System.out.println("1. Juego rapido");
+      System.out.println("2. Juego personalizado");
+      System.out.println("3. Salir");
+      int input = sc.nextInt();
+      if(input == 1) juegoRapido();
+      if(input == 2) juegoPersonalizado();
+      if(input == 3) break;
+    }
+  }
 
 }
