274cf853e8a6b713a3ee1a2157103a431cf30009
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-11 17:07:24 -0500

    Agrega la funcionalidad inicial

--- fase02/lab12/Videojuego.java
+++ fase02/lab12/Videojuego.java
@@ -6,5 +6,23 @@ public class VideoJuego {
     System.out.println("Para salir de la partida ingresa 'S'");
     String salir = sc.next();
     if(salir.toLowerCase().charAt(0) == 's') return;
+
+    Soldado[][] table = new Soldado[10][10];
+    ArrayList<Soldado> ejercito1 = new ArrayList<Soldado>();
+    ArrayList<Soldado> ejercito2 = new ArrayList<Soldado>();
+    fillTable(table, ejercito1, ejercito2);
+    printMayorNivel(table, ejercito1, ejercito2);
+    System.out.println("___________________________________");
+    System.out.println("IMPRIMIR PROMEDIO DE PUNTOS");
+    printPromedioPuntos(table, ejercito1, ejercito2);
+    System.out.println("___________________________________");
+    System.out.println("SOLDADOS ORDENADOS");
+    printSoladosOrdenados(table, ejercito1, ejercito2);
+    System.out.println("___________________________________");
+    System.out.println("RANKING DE PUNTOS EJERCITO1 POR BUBBLE SORT");
+    printRankingPointsBubble(table, ejercito1);
+    System.out.println("___________________________________");
+    System.out.println("RANKING DE PUNTOS EJERCITO2 POR SELECT SORT");
+    printRankingPointsSelect(table, ejercito2);
   }
 }
