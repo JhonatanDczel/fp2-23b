65dda3c925b0e4d40cbf5ced0bb19f6d01d54272
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-23 15:26:45 -0500

    Agrega la clase Punto con sus getters y setters

---
+++ practicas/prac03/ejercicio-01/Circulo.java
@@ -0,0 +1,19 @@
+public class Punto{
+  double x;
+  double y;
+  
+  public double getX(){
+    return this.x;
+  }
+  public double getY(){
+    return this.y;
+  }
+
+  public void setX(double coord){
+    this.x = coord;
+  }
+
+  public void setY(double coord){
+    this.y = coord;
+  }
+}
