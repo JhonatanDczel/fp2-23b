420058e902ef5831e358fef9521f80eb5a7c076a
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-23 22:42:36 -0500

    Agrega comentarios a Ejercicio1

--- fase02/trabajo03/Ejercicio1.java
+++ fase02/trabajo03/Ejercicio1.java
@@ -1,46 +1,56 @@
-class Punto{
+// Clase que representa un punto en un plano cartesiano
+class Punto {
   private double x;
   private double y;
 
-  public Punto(double x, double y){
+  // Constructor que inicializa las coordenadas (x, y) del punto
+  public Punto(double x, double y) {
     this.x = x;
-    this.y = y; 
-  }
-
-  public double distancia(Punto otroPunto){
-    return Math.sqrt(Math.pow(otroPunto.x - this.x, 2) + Math.pow(otroPunto.y - this.y, 2));
-  }
-
-  public double getX(){
-    return this.x;
-  }
-
-  public double getY(){
-    return this.y;
-  }
-
-  public void setY(double y){
     this.y = y;
   }
 
-  public void setX(double x){
+  // Método para calcular la distancia entre este punto y otro punto dado
+  public double distancia(Punto otroPunto) {
+    // Fórmula de distancia entre dos puntos en un plano cartesiano
+    return Math.sqrt(Math.pow(otroPunto.x - this.x, 2) + Math.pow(otroPunto.y - this.y, 2));
+  }
+
+  // Métodos para obtener las coordenadas x e y del punto
+  public double getX() {
+    return this.x;
+  }
+
+  public double getY() {
+    return this.y;
+  }
+
+  // Métodos para establecer las coordenadas x e y del punto
+  public void setY(double y) {
+    this.y = y;
+  }
+
+  public void setX(double x) {
     this.x = x;
   }
 }
 
-class Circulo extends Punto{
+// Clase que representa un círculo, hereda de la clase Punto
+class Circulo extends Punto {
   private double radio;
 
-  public Circulo(double x, double y, double radio){
-    super(x, y);
+  // Constructor que inicializa las coordenadas (x, y) y el radio del círculo
+  public Circulo(double x, double y, double radio) {
+    super(x, y); // Llamada al constructor de la clase base Punto
     this.radio = radio;
   }
 
-  public double getRadio(){
+  // Método para obtener el radio del círculo
+  public double getRadio() {
     return this.radio;
   }
 
-  public void setRadio(double radio){
+  // Método para establecer el radio del círculo
+  public void setRadio(double radio) {
     this.radio = radio;
   }
 }
