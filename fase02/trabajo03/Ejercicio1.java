f329404e63db6d927b34eb595f6489a0fde96a69
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-23 21:32:57 -0500

    Agrega setters y getters a la clase Punto

--- fase02/trabajo03/Ejercicio1.java
+++ fase02/trabajo03/Ejercicio1.java
@@ -6,4 +6,19 @@ class Punto{
     this.x = x;
     this.y = y; 
   }
+  public double getX(){
+    return this.x;
+  }
+
+  public double getY(){
+    return this.y;
+  }
+
+  public void setY(double y){
+    this.y = y;
+  }
+
+  public void setX(double x){
+    this.x = x;
+  }
 }
