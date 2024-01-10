6710689a4d6fbeb8bb450438b1ba030611703bdc
JhonatanDczel <jariasq@unsa.edu.pe>
2024-01-10 16:02:19 -0500

    Crea carpeta para la practica 01

---
+++ fase03/prac01/Sistema.java
@@ -0,0 +1,14 @@
+public class Sistema {
+  public static void main(String[] args) {
+    System.out.println("Sistema de Biblioteca EPIS");
+
+  }
+  public void login() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Iniciar sesión:");
+    System.out.println("Usuario :");
+    String user = sc.nextLine();
+    System.out.println("Password :");
+    String psw 
+  }
+}
