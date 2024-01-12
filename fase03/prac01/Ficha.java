15411330d056a7e7e385044fbc131d94968f9d43
Jorge <mamannihuarsaya4b@gmail.com>
2024-01-11 23:20:26 -0500

    La clase Ficha se encarga de juntar la informacion del usuario y el id del libro que se va a prestar, ademas de la fecha de inicio y de devolucion. Usamo la clase LocalDate para establecer la fecha. En java 21 los metodos de la clase Date estan absoletos

--- fase03/prac01/Ficha.java
+++ fase03/prac01/Ficha.java
@@ -1,3 +1,19 @@
+import java.time.*;
+
 public class Ficha {
-  public String id;
+  private String name, surname;
+  private int id;
+  private LocalDate startDate, returnDate; 
+  
+  public Ficha(int id, String name) {
+    this.name = name;
+    this.id = id;
+    startDate = LocalDate.now();
+    returnDate = startDate.plusDays(7);
+    System.out.println(startDate + ":" + returnDate);
+  } 
+
+  public static void main(String[] args) {
+    Ficha one = new Ficha("Jorge", "Mamani", 1829894);
+  }
 }
