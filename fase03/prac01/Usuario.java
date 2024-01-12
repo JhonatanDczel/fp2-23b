6bb6c3ebc7a040271cea9fa61c0594615ce696a1
JhossepV <jvelardesa@unsa.edu.pe>
2024-01-11 22:31:54 -0500

    update

---
+++ fase03/prac01/Usuario.java
@@ -0,0 +1,15 @@
+class Usuario{
+    private String ID;
+
+
+
+public void pedirLibro(String ID){
+    this.ID = ID;
+    
+}
+}
+/*Pedir libro(ID){
+    crear ficha
+        ID
+    //prestar libro
+}*/
