80afccf8fb279ad29b5890b959917429802981ff
JhonatanDczel <jariasq@unsa.edu.pe>
2024-01-15 14:37:46 -0500

    Agrega las importaciones de inicio

--- fase03/lab22/Videojuego.java
+++ fase03/lab22/Videojuego.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.SwingUtilities;
+
+
 public class Videojuego {
   public static void main(String[] args) {
     // Metodo principal de la clase
