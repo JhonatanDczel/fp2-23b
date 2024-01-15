8c10c5612c16e864bae9a9761a0da6cb42895a63
JhonatanDczel <jariasq@unsa.edu.pe>
2024-01-15 14:39:19 -0500

    Metodo para eliminar soldado y determinar ganador

--- fase03/lab22/Videojuego.java
+++ fase03/lab22/Videojuego.java
@@ -43,4 +43,38 @@ public class Videojuego {
       tablero[fila][columna]=s;
     }
   }
+
+  public static void eliminarSoldado(Soldado eliminar, ArrayList<Soldado> soldados) {
+    Iterator<Soldado> iterator = soldados.iterator();
+    while (iterator.hasNext()) {
+        Soldado entry = iterator.next();
+        if (entry.getNombre().equals(eliminar.getNombre())) {
+            iterator.remove();
+            break;
+        }
+    }
+  }
+  public static double promedioVida(Ejercito ejercito){
+    double promedio=0;
+    for(Soldado soldado: ejercito.iterar())
+      promedio+=soldado.getNivelVida();
+    return promedio/(double)ejercito.getTotalSoldados();
+  }
+  private static void determinarGanador(Ejercito ejercito1, Ejercito ejercito2, String reinoJugador1, String reinoJugador2) {
+    Random random=new Random();
+    System.out.println("\n---Metrica: Ejercito con mas soldados---");
+    double vida1=ejercito1.vidaTotalEjercito();
+    double vida2=ejercito2.vidaTotalEjercito();
+    System.out.printf("Ejercito 1: %-10s: %d   %.2f%% de probabilidad de victoria\n", reinoJugador1, ejercito1.vidaTotalEjercito(), (100/(vida1+vida2)*vida1));
+    System.out.printf("Ejercito 2: %-10s: %d   %.2f%% de probabilidad de victoria\n", reinoJugador2, ejercito2.vidaTotalEjercito(), (100/(vida1+vida2)*vida2));
+    double numGanador=random.nextDouble(100)+1;
+    String ganador="";
+    if(numGanador<=(100/(vida1+vida2)*vida1)){
+      ganador="Ejercito 1 de: "+reinoJugador1;      
+    }else if(numGanador>(100/(vida1+vida2)*vida2)){
+      ganador="Ejercito 2 de: "+reinoJugador2;
+    }
+    System.out.printf("\nEl ganador es el %s. Ya que al generar los\nporcentajes de probabilidad de victoria basada en los niveles de\nvida de sus soldados y aplicando un experimento aleatorio salió\nvencedor. (Aleatorio generado: %.2f%%)\n", ganador, numGanador);
+  }
 }
+
