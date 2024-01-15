190fb537141d550ccbd9a800a1c72b035e7f035b
JhonatanDczel <jariasq@unsa.edu.pe>
2024-01-15 14:39:46 -0500

    Metodo realizar accion

--- fase03/lab22/Videojuego.java
+++ fase03/lab22/Videojuego.java
@@ -76,5 +76,73 @@ public class Videojuego {
     }
     System.out.printf("\nEl ganador es el %s. Ya que al generar los\nporcentajes de probabilidad de victoria basada en los niveles de\nvida de sus soldados y aplicando un experimento aleatorio salió\nvencedor. (Aleatorio generado: %.2f%%)\n", ganador, numGanador);
   }
+
+  public static void realizarAccion(Soldado[][] tablero, Soldado soldado, ArrayList<Soldado> ejercito, ArrayList<Soldado> ejercitoE, Scanner sc){
+    Random random=new Random();
+    int fila=tablero.length;
+    int columna=tablero[0].length;
+
+    System.out.print("Escriba coordenadas(fila, columna): ");
+    int fila0=sc.nextInt();
+    int columna0=sc.nextInt();
+    if(fila0>=0 && fila0<fila && columna0>=0 && columna0<columna){
+      if(tablero[fila0][columna0]!=null){
+        System.out.println("*Enemigo encontrado*");
+        Soldado soldadoE=tablero[fila0][columna0];
+
+        if(soldado instanceof Caballero || soldado instanceof CaballeroFranco || soldado instanceof CaballeroMoro){
+          if(soldadoE instanceof Arquero){
+            if(soldado instanceof CaballeroFranco || soldado instanceof CaballeroMoro){ 
+              soldado.actualizarVida(2);
+            }else{soldado.actualizarDefensa(1);}}
+          if(soldadoE instanceof Lancero){
+            soldadoE.actualizarVida(1);}
+          if(soldadoE instanceof Espadachin){
+            soldado.actualizarVida(1);}
+          if(soldadoE instanceof CaballeroFranco || soldadoE instanceof CaballeroMoro){
+            soldadoE.actualizarVida(1);}
+        }
+        if(soldado instanceof Espadachin || soldado instanceof EspadachinConquistador || soldado instanceof EspadachinReal){
+          if(soldadoE instanceof Lancero){
+            if(soldado instanceof EspadachinConquistador || soldado instanceof EspadachinReal){
+              soldado.actualizarVida(2);}
+            else{soldado.actualizarVida(1);}}
+          if(soldadoE instanceof EspadachinConquistador || soldadoE instanceof EspadachinReal){
+            soldadoE.actualizarVida(1);}
+        }
+        if(soldado instanceof Arquero){
+          if(soldadoE instanceof Lancero){
+            soldado.actualizarVida(1);}
+        }    
+
+        double vida1=soldado.getvidaActual();
+        double vida2=soldadoE.getvidaActual();
+        System.out.println("Vida total de: "+soldado.getNombre()+"("+(int)vida1+")");
+        System.out.println("Vida total de: "+soldadoE.getNombre()+"("+(int)vida2+")");
+        int numGanador=(int)random.nextDouble(vida1+vida2)+1;
+        System.out.printf("Probabilidades de vencer: \n%s: %.2f%% | %s: %.2f%%\n", soldado.getNombre(),
+          (100/(vida1+vida2)*vida1), soldadoE.getNombre(), (100/(vida1+vida2)*vida2));
+
+        if(numGanador<=vida1){
+          System.out.println("*Enemigo derrotado*");
+          tablero[fila0][columna0]=soldado;
+          tablero[soldado.getFila()][soldado.getColumna()]=null;
+          soldado.setFila(fila0);
+          soldado.setColumna(columna0);
+          soldado.actualizarVida(1);
+          eliminarSoldado(soldadoE, ejercitoE);
+          
+        }else if(numGanador>vida1){
+          System.out.println("*Aliado derrotado*");
+          tablero[soldado.getFila()][soldado.getColumna()]=null;
+          soldadoE.actualizarVida(1);
+          eliminarSoldado(soldado, ejercito);
+          
+        }
+      }else
+        System.out.println("No se encontro enemigos\n");
+    }else
+      System.out.println("Coordenadas fuera del limite!!!");
+  }
 }
 
