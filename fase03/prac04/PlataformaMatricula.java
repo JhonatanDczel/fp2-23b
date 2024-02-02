7e5dee43abbf7176cca6e38ea6fde2af1886648b
JhonatanDczel <jariasq@unsa.edu.pe>
2024-02-02 00:30:45 -0500

    Avanzando con el menu

--- fase03/prac04/PlataformaMatricula.java
+++ fase03/prac04/PlataformaMatricula.java
@@ -1,40 +1,90 @@
+
+import java.util.Scanner;
+
 public class PlataformaMatricula {
-  public static ConectarDB datos;
-  public void mostrarDatos() {
-    //ConectarDB
-    /**
-     * mostrar :
-     * Respetar nombre de campos: id, cantidadCupos
-     * nombre de tabla: cursosSemestre
-     *
-     * 1 fundamentos de la progración - hora - turno - cupos
-     * 2 programación web - hora - turno - cupos
-     */
-  }
 
-  public void seleccionarCurso() {
-
-  }
-
-  public void cerrarSesion() {
-
-  }
-
-  public static void main(String args []) {
-    ConnectionDB baseDeDatos = ConnectionDB.getInstance();
-    //muestro los datos
-    ResultSet baseDeDatos.makeQuery("La consulta que desees");
-    //selecciono uno o mas cursos
-    Scanner sc = new Scanner(System.in);
-    int result = sc.nextInt();
-    while (result != 0 ) { //ya verificar ya forma de salid de este modo
-      //Puede ayudar el méotod isValidSelection()
-      //ser recomienda dejar toda esta parte como synchronized
-      result = sc.nextInt();
-      baseDeDatos.selectCourse(result);
+    public static void main(String args[]) {
+        System.out.println("+--------------------------------+");
+        System.out.println("|   Sistema de Matriculas EPIS   |");
+        System.out.println("+--------------------------------+");
+        menu();
     }
-    //se modifica la base de datos
-    datos.register();
-    //se matricula
+
+    public static void menu() {
+        System.out.println("+--------------------------------+");
+        System.out.println("|         Menu principal         |");
+        System.out.println("+--------------------------------+");
+        System.out.println("|  1. Empezar sesion             |");
+        System.out.println("|  2. Salir                      |");
+        System.out.println("+--------------------------------+");
+        System.out.print("|  Elige una opcion: ");
+
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+
+        System.out.println("+--------------------------------+");
+        sleep(500);
+
+        switch (op) {
+          case 1:
+            menuMatriculas();
+            break;
+          case 2:
+            System.out.println("Cerrando sesion...");
+            sleep(200);
+            return;
+        }
+
+        sleep(500);
+        menu();
+    }
+
+    public static void menuMatriculas(){
+        System.out.println("+--------------------------------+");
+        System.out.println("|         Menu Matriculas        |");
+        System.out.println("|      Tiempo restante:          |");
+        System.out.println("+--------------------------------+");
+        System.out.println("|  1. Mostrar cupos              |");
+        System.out.println("|  2. Registrar matricula        |");
+        System.out.println("|  3. Regresar al menu principal |");
+        System.out.println("+--------------------------------+");
+        System.out.print("|  Elige una opcion: ");
+
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+
+        System.out.println("+--------------------------------+");
+        sleep(500);
+
+        switch (op) {
+          case 1:
+            //Logica para mostrar cupos
+            break;
+          case 2:
+            //Logica para registrar matricula
+            break;
+          case 3:
+            return;
+          default:
+            break;
+        }
+        menuMatriculas();
+    }
+
+  public static void sleep(int n) {
+    try {
+      Thread.sleep(n);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
+  public static void sleep(){
+    try {
+      Thread.sleep(800);
+    } catch (InterruptedException e) {
+      e.printStackTrace(); 
+    }
+  }
+
 }
+
